4372d1f2bcd96e814e54c128218c04219a74d945
AminulHaqueShihab <61130539+AminulHaqueShihab@users.noreply.github.com>
2023-03-01 13:13:31 +0600

    Update Home.js

--- screens/Home.js
+++ screens/Home.js
@@ -1,4 +1,4 @@
-import { View, Text, FlatList, StyleSheet, Keyboard, TouchableOpacity, TextInput } from 'react-native';
+import { View, Text, FlatList, StyleSheet, Keyboard, TouchableOpacity, TextInput, Pressable } from 'react-native';
 import React, { useState, useEffect } from 'react';
 import { FontAwesome } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
@@ -61,6 +61,106 @@ const Home = () => {
                 })
         }
     }
+
+    return (
+        <View style={{flex: 1, backgroundColor: '#fff'}}>
+            <View style={styles.formContainer}>
+                <TextInput
+                    style={styles.input}
+                    placeholder='Add New Task'
+                    placeholderTextColor='#aaaaaa'
+                    onChangeText={(heading) => setAddData(heading)}
+                    value={addData}
+                    underlineColorAndroid='transparent'
+                    autoCapitalize='none'
+                />
+                <TouchableOpacity style={styles.button} onPress={addTodo}>
+                    <Text style={styles.buttonText}>+</Text>
+                </TouchableOpacity>
+            </View>
+            <FlatList
+                data={todos}
+                numColumns={1}
+                renderItem={({item}) => (
+                    <View>
+                        <Pressable
+                            style={styles.container}
+                            onPress={() => navigation.navigate('Detail', {item})}
+                        >
+                            <FontAwesome
+                                name='trash-o'
+                                color='red'
+                                opPress={() => deleteTodo(item)}
+                            />
+                            <View style={styles.innerContainer}>
+                                <Text style={styles.itemHeading}>
+                                    {item.heading[0].toUpperCase() + item.heading.slice(1)}
+                                </Text>
+                            </View>
+                        </Pressable>
+                    </View>
+                )}
+            />
+
+        </View>
+    )
 }
 
 export default Home;
+
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor:'#e5e5e5',
+        padding: 15,
+        borderRadius: 15,
+        margin: 5,
+        marginHorizontal: 10,
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    innerContainer: {
+        alignItems: 'center',
+        flexDirection: 'column',
+        marginLeft: 45,
+    },
+    itemHeading: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        marginRight:22,
+    },
+    formContainer: {
+        flexDirection: 'row',
+        height: 80,
+        marginLeft: 10,
+        marginRight: 10,
+        marginTop: 100,
+    },
+    input: {
+        height: 48,
+        borderRadius: 5,
+        overflow: 'hidden',
+        backgroundColor: 'white',
+        paddingLeft: 16,
+        flex: 1,
+        marginRight: 5,
+    },
+    button: {
+        height: 47,
+        borderRadius: 5,
+        backgroundColor: '#788eec',
+        width: 80,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    buttonText: {
+        color: 'white',
+        fontSize: 30,
+        fontWeight: 'bold',
+    },
+    todoIcon: {
+        marginTop: 5,
+        fontSize: 20,
+        marginLeft: 14,
+    },
+})
